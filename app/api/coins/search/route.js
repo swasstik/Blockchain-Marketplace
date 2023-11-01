350f46f64b8c849a539d4253d99e5219f7200879
Swastik <bairagiswastik@gmail.com>
2023-11-01 15:15:36 +0530

    updated

--- app/api/coins/search/route.js
+++ app/api/coins/search/route.js
@@ -1,6 +1,6 @@
 
 import { NextResponse } from "next/server";
-// import { useRouter } from "next/navigation";
+
 
 async function fetchCoins(){
     const response = await fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', 
@@ -16,11 +16,10 @@ async function fetchCoins(){
 }
 
 export async function GET(request){
-    // const router = useRouter()
+   
     const coins = await fetchCoins();
     const {searchParams} = new URL(request.url);
-    console.log(request.url)
-    // router.push(request.url);
+    
     const query = searchParams.get('query');
     
     const filteredCoins = coins.data.coins.filter((coin)=>{
